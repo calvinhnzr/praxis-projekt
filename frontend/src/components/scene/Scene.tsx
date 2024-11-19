f3ea0ea9d1501245f78dcfdf5f40513a0d0222b2
Calvin Hinze <calvinhinzer@gmail.com>
2024-11-19 13:17:20 +0100

    [TASK] grid layout

--- frontend/src/components/scene/Scene.tsx
+++ frontend/src/components/scene/Scene.tsx
@@ -9,17 +9,34 @@ import {
   Stage,
   Grid,
 } from "@react-three/drei"
+import { useControls } from "leva"
 
 const Scene = () => {
+  const { gridSize, ...gridConfig } = useControls({
+    gridSize: [10.5, 10.5],
+    cellSize: { value: 0.5, min: 0, max: 10, step: 0.1 },
+    cellThickness: { value: 1, min: 0, max: 5, step: 0.1 },
+    cellColor: "#6f6f6f",
+    sectionSize: { value: 3, min: 0, max: 10, step: 0.1 },
+    sectionThickness: { value: 1.5, min: 0, max: 5, step: 0.1 },
+    sectionColor: "#cd4d4d",
+    fadeDistance: { value: 25, min: 0, max: 100, step: 1 },
+    fadeStrength: { value: 1, min: 0, max: 1, step: 0.1 },
+    followCamera: false,
+    infiniteGrid: true,
+  })
+
   return (
-    <Canvas>
+    <Canvas shadows camera={{ position: [10, 12, 12], fov: 25 }}>
       <ambientLight intensity={Math.PI / 2} />
-      <Grid scale={100} />
+      <Grid position={[0, -0.01, 0]} args={gridSize} {...gridConfig} />
 
       <CameraControls />
-      <Box args={[100, 294, 100]} position={[0, 0, 0]}>
-        <meshStandardMaterial attach="material" color="blue" />
-      </Box>
+      <group>
+        <Plane args={[1, 1]} rotation={[-Math.PI / 2, 0, 0]}>
+          <meshStandardMaterial color="#CED8F7" />
+        </Plane>
+      </group>
     </Canvas>
   )
 }
---
+++ frontend/src/store/base.tsx
@@ -0,0 +1,30 @@
+// TH Köln Color Palette
+const th_color_red = "#CF1820"
+const th_color_orange = "#EC6525"
+const th_color_purple = "#AF368C"
+
+// Amazing Bomberman Color Palette
+const bm_color_yellow = "#ffca81"
+const bm_color_yellow_light = "#dfc5a4"
+const bm_color_orange = "#e59872"
+const bm_color_blue_night = "#2b3756"
+const bm_color_gray_dark = "#4c5869"
+const bm_color_gray = "#626f82"
+const bm_color_red = "#cd4d4d"
+const bm_color_green = "#81ad97"
+const bm_color_green_light = "#a8c6ba"
+
+// Blueprints Color Palette
+const bp_color_blue = "#3057E1"
+const bp_color_blue_pale = "#4A6DE5"
+const bp_color_blue_light = "#CED8F7"
+const bp_color_blue_dark = "#002082"
+
+// EXPORTS
+export const base_color_black = "#000000"
+export const base_color_white = "#FFFFFF"
+export const base_color_background = bp_color_blue_pale
+export const base_color_accent = bp_color_blue_light
+
+// SPACING
+export const GAP_EXPLOSION = 10 //rem
