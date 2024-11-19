0c36f580149d4b9d21d51a2f541f2fd47ec786d7
Calvin Hinze <calvinhinzer@gmail.com>
2024-11-19 23:07:07 +0100

    [TASK] custom grid shader

--- frontend/src/components/scene/Scene.tsx
+++ frontend/src/components/scene/Scene.tsx
@@ -8,7 +8,6 @@ import {
   PerspectiveCamera,
   Plane,
   Stage,
-  Grid,
   Center,
   GizmoHelper,
   GizmoViewport,
@@ -21,43 +20,56 @@ import {
   BakeShadows,
 } from "@react-three/drei"
 
+import { Grid } from "@/utils/Grid"
+
 import SpruceTree from "./models/SpruceTree"
 
 import { useControls, Leva } from "leva"
 
 export const Scene = () => {
   const { gridSize, ...gridConfig } = useControls({
-    gridSize: [10, 10],
+    gridSize: [100, 100],
     cellSize: { value: 1.2, min: 0, max: 10, step: 0.6 },
     cellThickness: { value: 1, min: 0, max: 5, step: 0.1 },
     cellColor: "#6f6f6f",
     sectionSize: { value: 1.2 * 5, min: 0, max: 10, step: 0.6 },
     sectionThickness: { value: 1.5, min: 0, max: 5, step: 0.1 },
     sectionColor: "#cd4d4d",
-    fadeDistance: { value: 200, min: 0, max: 100, step: 1 },
+    fadeDistance: { value: 400, min: 0, max: 100, step: 1 },
     fadeStrength: { value: 1, min: 0, max: 1, step: 0.1 },
     followCamera: false,
     infiniteGrid: true,
   })
 
+  const maxHeight = 3.88 * 3 + 4.25
+  const maxWidth = 24 * 6 + 8.4
+  const maxDepth = 8.05 + 7.6
+
   return (
     <Canvas
       shadows
       // orthographic
-      camera={{
-        // position: [0, 12, 0],
-        position: [10, 12, 12],
-        rotation: [-Math.PI / 2, 0, 0],
-        fov: 25,
-        // zoom: 100,
-      }}
+      // camera={{
+      //   // position: [0, 12, 0],
+      //   position: [10, 15, 12],
+      //   rotation: [-Math.PI / 2, 0, 0],
+      //   // fov: 25,
+      //   zoom: 40,
+      // }}
     >
+      <group scale={1}>
+        <mesh position={[6, maxHeight / 2, 0]} castShadow visible={false}>
+          <boxGeometry args={[maxDepth + 2 * 1.85, maxHeight, maxWidth]} />
+          <meshStandardMaterial color="cornflowerblue" />
+        </mesh>
+      </group>
+
       <mesh position={[6, 3.62 / 2, 0]} castShadow visible={true}>
         <boxGeometry args={[6, 3.62, 8.525]} />
         <meshStandardMaterial color="cornflowerblue" />
       </mesh>
 
-      <SpruceTree position={[-2, 0, 0]} scale={0.2} />
+      <SpruceTree position={[-2, 0.05, 0]} scale={0.2} />
 
       <group position={[3, 0, 0.6]} visible={false}>
         <mesh rotation={[-Math.PI / 2, 0, 0]}>
@@ -67,32 +79,24 @@ export const Scene = () => {
       </group>
 
       <group>
-        {/* <Leva hidden /> */}
+        <Leva hidden />
+
+        <OrthographicCamera
+          position={[0, 100, 0]}
+          rotation={[0, Math.PI / -2, 0]}
+          zoom={15}
+          makeDefault
+        />
         <MapControls
-          enableRotate={true}
+          // enableRotate={true}
           minPolarAngle={0}
           maxPolarAngle={Math.PI}
         />
 
-        <Environment preset="city" />
+        <Environment preset="dawn" />
       </group>
 
       <group>
-        <group visible={false}>
-          <Center top position={[-2, 0, 2]}>
-            <mesh castShadow>
-              <sphereGeometry args={[0.5, 64, 64]} />
-              <meshStandardMaterial color="coral" />
-            </mesh>
-          </Center>
-
-          <Center top position={[2.5, 0, 2]}>
-            <mesh castShadow rotation={[0, Math.PI / 4, 0]}>
-              <boxGeometry args={[0.7, 0.7, 0.7]} />
-              <meshStandardMaterial color="cornflowerblue" />
-            </mesh>
-          </Center>
-        </group>
         <Grid position={[0, -0.01, 0]} args={gridSize} {...gridConfig} />
         <Shadows />
       </group>
